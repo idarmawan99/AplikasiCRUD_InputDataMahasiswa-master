da48d7bdf14404a9ec3e3d73cc75d4ef0df2f3ca
idarmawan99 <iwandarmawan9988@gmail.com>
2022-04-09 05:03:51 +0700

    Initial commit

---
+++ app/src/main/java/com/idarmawan/InputDataMahasiswa/InformasiActivity.java
@@ -0,0 +1,13 @@
+package com.idarmawan.InputDataMahasiswa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi);
+    }
+}
